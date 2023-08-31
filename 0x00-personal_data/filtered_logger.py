3dd03ef728ccb761263f46358e236248b9891d1a
vpnchengo <charo.alvin@gmail.com>
2023-08-31 18:20:00 +0300

    task2 creating logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,9 @@ import re
 import logging
 
 
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
     """
@@ -20,6 +23,22 @@ class RedactingFormatter(logging.Formatter):
         """Returns filtered values from log records"""
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """function that returns a logging.Logger object"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    target_handler = logging.StreamHandler()
+    target_handler.setLevel(logging.INFO)
+
+    formatter = RedactingFormatter(PII_FIELDS)
+    target_handler.setFormatter(formatter)
+
+    logger.addHandler(target_handler)
+    return logger
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,

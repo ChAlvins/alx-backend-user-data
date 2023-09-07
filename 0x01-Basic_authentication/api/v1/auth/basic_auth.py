c02e942c9a60502a1312fefc4eeb57bbd29cab2f
vpnchengo <charo.alvin@gmail.com>
2023-09-07 10:20:43 +0300

    basic user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -33,3 +33,20 @@ class BasicAuth(Auth):
             return decoded_value.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        Extract and return the user email and password from
+        a decoded Base64 authorization header
+        returns a tuple with two values: (email, password)
+        """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
+
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return (email, password)

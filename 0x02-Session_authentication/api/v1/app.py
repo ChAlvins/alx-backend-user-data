872a777256816025f7e088ece8ae80c98e19fa39
vpnchengo <charo.alvin@gmail.com>
2023-09-08 04:32:59 +0300

    updated @app.before_request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -37,6 +37,9 @@ def before_request():
         if auth.require_auth(request.path, excluded_paths):
             if auth.authorization_header(request) is None:
                 abort(401, description="Unauthorized")
+
+            request.current_user = auth.current_user(request)
+
             if auth.current_user(request) is None:
                 abort(403, description="Forbidden")
 

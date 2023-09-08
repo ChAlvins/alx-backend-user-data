6a52ed1a09a8f2f689c4ea0ac395300b52d03d61
vpnchengo <charo.alvin@gmail.com>
2023-09-08 04:56:28 +0300

    updated session auth class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 """Definition of class SessionAuth"""
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """Implement Session Authorization protocol methods"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Create a Session ID for a user_id
+        :param user_id: User ID (string) to associate with the session
+        :return: Session ID (string)
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
